b6e4a38c03990f30865244b22bd770db12fa96ff
Yash Dave <yashdave307@gmail.com>
2023-04-27 12:26:48 +0530

    About and policy page added.

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -5,7 +5,24 @@ import Layout from '../components/Layout/Layout'
 const Policy = () => {
   return (
     <Layout>
-        <div>Policy</div>
+        <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>privacy policy 1</p>
+          <p>privacy policy 2</p>
+          <p>privacy policy 3</p>
+          <p>privacy policy 4</p>
+          <p>privacy policy 5</p>
+          <p>privacy policy 6</p>
+          <p>privacy policy 7</p>
+        </div>
+      </div>
     </Layout>
   )
 }
